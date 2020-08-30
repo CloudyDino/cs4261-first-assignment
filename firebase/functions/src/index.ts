8b4db114c0d7a09f6fbfb276f6e2575d0361af97
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-08-30 13:14:04 -0400

    Create rest api function for getting all messages

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send('Hello from Firebase!\n\n');
+});
+
+export const getAllMessages = functions.https.onRequest((request, response) => {
+    const db = admin.database();
+    const ref = db.ref("messages");
+    ref.on("value", function (data) {
+        response.send(data);
+    });
+});
