c09cf4f0883dcd5b184e7479d56f48f42af3a0d7
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-09-01 22:12:01 -0400

    Convert all java code to kotlin
    Had to specifically clean up FriendlyMessage after the conversion to make it work with the app

---
+++ android/app/src/main/java/com/google/firebase/codelab/friendlychat/DownloadMessagesActivity.kt
@@ -0,0 +1,11 @@
+package com.google.firebase.codelab.friendlychat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DownloadMessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download_messages)
+    }
+}
