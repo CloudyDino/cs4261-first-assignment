ff3f5385a6a408213989f415d7f774b84ed8199a
Amaan Marfatia <amaanmarfatia@gmail.com>
2020-09-02 01:55:41 -0400

    Filter out unwanted information

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -10,7 +10,22 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 export const getAllMessages = functions.https.onRequest((request, response) => {
     const db = admin.database();
     const ref = db.ref("messages");
-    ref.on("value", function (data) {
+    ref.on("value", function (snapshot) {
+        const data = snapshot.val();
+        for (const messageId in data) {
+            if (!request.body['getAvatar']) {
+                delete data[messageId]['photoUrl']
+            }
+            if (!request.body['getImage']) {
+                delete data[messageId]['imageUrl']
+            }
+            if (!request.body['getName']) {
+                delete data[messageId]['name']
+            }
+            if (!request.body['getText']) {
+                delete data[messageId]['text']
+            }
+        }
         response.send(data);
     });
 });
